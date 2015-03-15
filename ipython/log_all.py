17420fcc0342d2965996720776099c8eb1eb99c3
John Lockwood <elitepropertiesbroker@gmail.com>
2015-03-14 21:53:26 -0700

    Link to an IPython notebook in readme

--- ipython/log_all.py
+++ ipython/log_all.py
@@ -16,3 +16,28 @@ quit()
 get_ipython().system(u'subl ../README.md')
 get_ipython().magic(u'pwd ')
 quit()
+
+quit()
+
+print('hi Jenniffer')
+get_ipython().magic(u'cat log_all.py')
+print('hi again')
+get_ipython().magic(u'cat log_all.py')
+quit()
+
+
+get_ipython().magic(u"logstart 'delme.py'")
+quit()
+
+myfiles = get_ipython().getoutput(u'ls')
+myfiles
+quit()
+
+myfiles = get_ipython().getoutput(u'ls')
+myfiles.n
+myfiles.p
+myfiles.s
+pyvar = 'hello world!'
+get_ipython().system(u'echo "A python variable: {pyvar}"')
+quit()
+
